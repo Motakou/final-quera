c702bd32111fb24b8bac8520ca0a8861aab5d24d
Safa_khalili <135706263+Safakhalili@users.noreply.github.com>
2024-03-16 08:38:15 +0330

    Add files via upload

--- final_quera/asgi.py
+++ final_quera/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for final_quera project.
+ASGI config for Imdb project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final_quera.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Imdb.settings')
 
 application = get_asgi_application()
--- final_quera/urls.py
+++ final_quera/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for final_quera project.
+URL configuration for Imdb project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
